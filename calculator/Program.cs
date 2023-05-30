6e34ad9528a401b864387e1332161778c0a6c8ce
or-temka <tema.chegortzov@mail.ru>
2023-05-30 19:30:58 +0300

    new update - added exponentiation

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -14,14 +14,22 @@ namespace calculator
             sNum = Convert.ToDouble(Console.ReadLine());
             
             Console.WriteLine("Выберите тип операции для a и b:");
-            Console.WriteLine("1. Сложение\n2. Вычетание\n3. Умножение\n4. Деление");
+            Console.WriteLine("1. Сложение\n2. Вычетание\n3. Умножение\n4. Деление\n5. Возведение в степень");
             typeOperation = Convert.ToInt32(Console.ReadLine());
             switch (typeOperation)
             {
                 case 1: result = fNum + sNum; break;
                 case 2: result = fNum - sNum; break;
                 case 3: result = fNum * sNum; break;
-                case 4: result = fNum / sNum; break;
+                case 4:
+                    if (sNum == 0)
+                    {
+                        Console.WriteLine("Деление на 0 невозможно!");
+                        return;
+                    }
+                    result = fNum / sNum;
+                    break;
+                case 5: result = Math.Pow(fNum, sNum); break;
             }
             Console.Write("Ответ: " + result);
         }
