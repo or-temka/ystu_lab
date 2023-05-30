d19b09da2dc856c63606a62059420ea882090b3c
or-temka <tema.chegortzov@mail.ru>
2023-05-30 18:42:38 +0300

    new files: .gitignore && C# project

---
+++ calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fNum, sNum, result = 0;
+            int typeOperation;
+            Console.Write("Введите первое число >> ");
+            fNum = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число >> ");
+            sNum = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Выберите тип операции для a и b:");
+            Console.WriteLine("1. Сложение\n2. Вычетание\n3. Умножение\n4. Деление");
+            typeOperation = Convert.ToInt32(Console.ReadLine());
+            switch (typeOperation)
+            {
+                case 1: result = fNum + sNum; break;
+                case 2: result = fNum - sNum; break;
+                case 3: result = fNum * sNum; break;
+                case 4: result = fNum / sNum; break;
+            }
+            Console.Write("Ответ: " + result);
+        }
+    }
+}
